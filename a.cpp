a9b1ff293266bc2d6775c852446decdfe3f20a0b
mengxain0913 <mengxian0913@gmail.com>
2023-08-15 03:04:01 +0800

    upload

---
+++ a.cpp
@@ -0,0 +1,28 @@
+/*********************************************************
+# File Name: a.cpp
+# Author: Vincent Yang
+# Mail: mengxian0913@gmail.com
+# Created Time: 二  8/15 03:03:57 2023
+*********************************************************/
+
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ff first
+#define ss second
+#define fastIO cin.tie(nullptr)->sync_with_stdio(false);
+#define INF 0x7FFFFFFF
+#define pb push_back
+#define all(aa) aa.begin(),aa.end()
+#define MOD 1e9+7
+
+void solve(){
+    
+}
+
+signed main(){
+    fastIO
+    solve();
+    return 0;
+}
