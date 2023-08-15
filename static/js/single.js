1e2e564e7e9f40f1d8e71efd6937e8c1c0da0a1f
mengxain0913 <mengxian0913@gmail.com>
2023-08-15 16:15:02 +0800

    update

--- static/js/single.js
+++ static/js/single.js
@@ -47,3 +47,39 @@ window.addEventListener('load', function(){
     UnloadContainer.style.height = "auto";
     loadingInProgress = false;
 });
+
+
+
+const ToTopArrow = document.querySelector(".imgcontainer");
+let CurrentScrollY = -1;
+// 監聽網頁的滾動事件
+window.addEventListener('scroll', function() {
+    // 獲取目前的捲軸位置
+    var scrollY = window.pageYOffset;
+  
+    // 如果捲軸位置大於0，表示正在向上滾動
+    if (scrollY < CurrentScrollY) {
+        ToTopArrow.style.width = "65px";
+        ToTopArrow.style.height = "65px";
+        ToTopArrow.style.transform =  "rotate(360deg)";
+    }
+
+    else{
+        ToTopArrow.style.width = "0px";
+        ToTopArrow.style.height = "0px";
+        ToTopArrow.style.transform =  "rotate(-360deg)";
+    }
+
+    CurrentScrollY = scrollY;
+});
+
+
+ToTopArrow.addEventListener('click', function() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+  
+
