72149a66ae1b33a5bb56368ed9fba85f2a8a5c38
mengxain0913 <mengxian0913@gmail.com>
2023-08-24 15:51:27 +0800

    update

--- static/js/catalog.js
+++ static/js/catalog.js
@@ -1,32 +1,87 @@
-_containerSelector = '#markdown-content';
+function generateCatalog(selector) {
 
-function generateCatalog(_selector){
-    var P = $(_containerSelector);
+    _containerSelector = '#markdown-content'
 
-    a = P.find('h1, h2, h3, h4, h5, h6');
-    $(_selector).html("");
+    // init
+    var P = $(_containerSelector), a, n, t, l, i, c;
+    a = P.find('h1,h2,h3,h4,h5,h6');
 
-    a.each(function(){
+    // clean
+    $(selector).html('')
+
+    // appending
+    a.each(function () {
         n = $(this).prop('tagName').toLowerCase();
         i = "#" + $(this).prop('id');
         t = $(this).text();
         c = $(`<a aria-label="${t}" href="${i}" rel="nofollow">${t}</a>`);
         l = $(`<li class="${n}_nav"></li>`).append(c);
-        $(_selector).append(l);    
+        $(selector).append(l);
     })
-
     return a;
 }
 
-const sideElements = generateCatalog('ul.catalog-ul');
+const sideElements = generateCatalog("ul.catalog-ul"), all_htags = []
+let firstTag = null
+sideElements.each((index, element) => {
+    const name = element.id
+    const innerTagsCount = 0
+    const sideElement = $(`[aria-label="${element.innerText}"]`)
+    const top =  $(`[id="${name}"]`).offset().top
+    const htag = sideElement.parent()
+    const htag_className = $(htag[0]).attr("class");
+    const innerTags = htag.nextUntil(`[class="${htag_className}"]`)
+
+    let innerFirstTag = null, i = 0
+    while(!innerFirstTag && i < innerTags.length){
+        if(innerTags[i].className > htag[0].className) innerFirstTag = innerTags[i].className
+        i++
+    }
+
+    const tagName = $(`[id="${name}"]`).prop("tagName")
+    if(!firstTag) firstTag = tagName
+    if(tagName !== firstTag) sideElement.hide()
+
+    all_htags.push({ sideElement, innerTags, innerFirstTag, innerTagsCount, top, tagName })
+})
+
+let count = [0, 0, 0, 0, 0]
+for(let i = all_htags.length - 1; i >= 0; i--){
+    const index = parseInt(all_htags[i].tagName.slice(-1))
+    all_htags[i].innerTagsCount = 1
+    count[index - 1] += 1
+    if(i !== all_htags.length - 1 && index !== 5){
+        for(let j = index; j < 5; j++){
+            all_htags[i].innerTagsCount += count[j]
+            count[index - 1] += count[j]
+            count[j] = 0
+        }
+    }
+}
 
 
-
-// sideElements.each((index, element) => {
-//     const name = element.id
-//     const innerTagsCount = 0 // 子標題數量預設為 0 
-//     const sideElement = $(`[aria-label="${element.innerText}"]`) // 快速連結中的標題元素
-//     const top =  $(`[id="${name}"]`).offset().top // 文章中標題位置
-//     const htag = sideElement.parent();
-//     const innerTags = htag.nextUntil(`[class="${htag[0].className}"]`) // 標題層級下的所有子標題元素
-// })
+$(window).scroll(() => {
+    let curPos = window.pageYOffset
+    let isBottom = parseInt(curPos + document.documentElement.clientHeight) === document.documentElement.scrollHeight
+    all_htags.forEach((item, index) => {
+        const {sideElement, innerTags, innerFirstTag, innerTagsCount, top} = item
+        if(index < all_htags.length - 1){
+            if(innerTagsCount > 1){
+                if(curPos >= top && curPos < all_htags[index + innerTagsCount - 1].top && !isBottom){
+                    sideElement.attr("style", "color: #337ab7")
+                    innerTags.filter((index, tag) => tag.className === innerFirstTag).slideDown()
+                }
+                else{ // 收起來
+                    sideElement.removeAttr("style", "color")
+                    innerTags.filter((index, tag) => tag.className === innerFirstTag).slideUp()
+                }
+            }else{
+                if(curPos >= top && curPos < all_htags[index + innerTagsCount].top && !isBottom) sideElement.attr("style", "color: #337ab7")
+                else sideElement.removeAttr("style", "color")
+            }
+        }else{
+            if(curPos >= top || isBottom) sideElement.attr("style", "color: #337ab7")
+            else sideElement.removeAttr("style", "color")
+        }
+    })
+})
