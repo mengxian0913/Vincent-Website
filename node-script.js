92071320f77d250c578921778b0b209af9679217
mengxain0913 <mengxian0913@gmail.com>
2023-11-25 17:31:12 +0800

    update

--- node-script.js
+++ node-script.js
@@ -1,9 +1,9 @@
-const fs = require('fs');
-const path = require('path');
-const matter = require('gray-matter');
+const fs = require("fs");
+const path = require("path");
+const matter = require("gray-matter");
 
-const postsFolderPath = path.join(__dirname, 'content', 'posts');
-const jsonFilePath = path.join(__dirname, 'static/json', 'posts.json');
+const postsFolderPath = path.join(__dirname, "content", "posts");
+const jsonFilePath = path.join(__dirname, "static/json", "posts.json");
 const jsonContent = [];
 const baseURL = "https://vincent-s-blog.web.app/posts/";
 
@@ -12,13 +12,13 @@ console.log(postsFolderPath);
 const items = fs.readdirSync(postsFolderPath);
 console.log(items);
 
-function GetAllPost(now_path, currentURL){
-  fs.readdirSync(now_path).forEach(file => {
+function GetAllPost(now_path, currentURL) {
+  fs.readdirSync(now_path).forEach((file) => {
     const filePath = path.join(now_path, file);
-    const fileNameWithoutExtension = file.replace(/\.[^.]+$/, '');
+    const fileNameWithoutExtension = file.replace(/\.[^.]+$/, "");
     // 檢查是否為檔案
-    if(fs.statSync(filePath).isFile()){
-      const fileContent = fs.readFileSync(filePath, 'utf8');
+    if (fs.statSync(filePath).isFile()) {
+      const fileContent = fs.readFileSync(filePath, "utf8");
       const parsedContent = matter(fileContent);
       console.log(parsedContent.data.title, " ", parsedContent.data.tags);
       jsonContent.push({
@@ -28,12 +28,9 @@ function GetAllPost(now_path, currentURL){
         content: parsedContent.content,
         url: currentURL + fileNameWithoutExtension + "/",
       });
-    }
-
-    else{
+    } else {
       GetAllPost(filePath, currentURL + fileNameWithoutExtension + "/");
     }
-
   });
 
   return;
@@ -41,13 +38,12 @@ function GetAllPost(now_path, currentURL){
 
 GetAllPost(postsFolderPath, baseURL);
 
-
 const jsonString = JSON.stringify(jsonContent, null, 2);
 
-fs.writeFile(jsonFilePath, jsonString, 'utf8', (writeErr) => {
+fs.writeFile(jsonFilePath, jsonString, "utf8", (writeErr) => {
   if (writeErr) {
-    console.error('Error writing JSON file:', writeErr);
+    console.error("Error writing JSON file:", writeErr);
   } else {
-    console.log('JSON file written successfully:', jsonFilePath);
+    console.log("JSON file written successfully:", jsonFilePath);
   }
 });
